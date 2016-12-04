ddf311d6d3e00fc527af2651ddbe6fdbd8ce2fcc
Pavel Naydenov <naydenov.p.v@gmail.com>
2016-12-04 19:43:41 +0300

    bootstrap templates

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'sidekiq'
 
 gem 'rmagick', require: false
+
+gem 'bootstrap-sass', '~> 3.3.6'
--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,4 +2,17 @@ module VideosHelper
   def filepath_url filepath
     filepath.to_s.sub(Rails.public_path.to_s, '')
   end
+
+  def video_status_color video
+    case video.status.to_sym
+      when :inited
+        'gray'
+      when :uploaded
+        'darkblue'
+      when :previewed
+        'blue'
+      when :encoded
+        'forestgreen'
+    end
+  end
 end
